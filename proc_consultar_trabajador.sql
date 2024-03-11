8826c77ede5d9cafb5f9536d126baf0ca0aa0196
WolverinLando <orlandourbanotrejo@gmail.com>
2024-03-11 14:22:53 -0600

    Procedimiento para consultar informacion de un trabajador

---
+++ proc_consultar_trabajador.sql
@@ -0,0 +1,25 @@
+--Eliminar procedure consultar
+DROP PROCEDURE IF EXISTS proc_consultar_trabajador;
+
+--Creacion del procedure
+DELIMITER //
+CREATE PROCEDURE proc_consultar_trabajador()
+BEGIN
+   SELECT
+      nombre,
+      apellido_paterno,
+      apellido_materno,
+      sueldo,
+      direccion,
+      correo,
+      telefono,
+      puesto
+      FROM
+      trabajador
+      INNER JOIN puesto ON trabajador.id_puesto = puesto.id_puesto;
+END //
+DELIMITER ;
+
+--Llamada al procedure
+CALL proc_consultar_trabajador();
+
