054aef495faefc47ec6bc63cdd5cad0e065e4983
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-19 17:31:52 -0600

    Modificaciones

--- usuario.sql
+++ usuario.sql
@@ -1,6 +1,12 @@
 --- Creacion de la base de datos --
 CREATE DATABASE truper;
 
+CREATE USER 'orlando'@'%' IDENTIFIED BY 'database';
+
+GRANT ALL PRIVILEGES ON truper.* TO 'orlando'@'%';
+
+FLUSH PRIVILEGES;
+
 --Usar la base de datos ---
 USE truper;
 
