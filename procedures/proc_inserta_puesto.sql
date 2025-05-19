ff902a355714aff4d1a8c810865b52038aef2aa9
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-19 17:20:17 -0600

    Procedure insertar puesto

---
+++ procedures/proc_inserta_puesto.sql
@@ -0,0 +1,37 @@
+------Creacion de procedure para inserta un puesto
+DELIMITER //
+CREATE PROCEDURE proc_inserta_puesto(
+   IN p_puesto VARCHAR(50)
+)
+BEGIN
+   INSERT INTO puesto (puesto) VALUES (p_puesto);
+END //
+DELIMITER ;
+
+--Eliminar procedure
+DROP PROCEDURE IF EXISTS proc_inserta_puesto;
+
+---Llamada al procedure-------
+CALL proc_inserta_puesto('Auxiliar Invetarios');
+CALL proc_inserta_puesto('Operador de transporte de personal');
+CALL proc_inserta_puesto('Supervisor de seguridad');
+CALL proc_inserta_puesto('Verificador de productos');
+CALL proc_inserta_puesto('Gerente de proyectos');
+CALL proc_inserta_puesto('Facilitador Ambiental');
+CALL proc_inserta_puesto('Monitorista');
+CALL proc_inserta_puesto('Analista tráfico');
+CALL proc_inserta_puesto('Auditor JR');
+CALL proc_inserta_puesto('Checador');
+
+--- Insertar datos de manera normal ---
+INSERT INTO puesto (puesto) VALUES
+   ('Auxiliar Invetarios'),
+   ('Operador de transporte de personal'),
+   ('Supervisor de seguridad'),
+   ('Verificador de productos'),
+   ('Gerente de proyectos'),
+   ('Facilitador Ambiental'),
+   ('Monitorista'),
+   ('Analista tráfico'),
+   ('AuditorJR');
+
