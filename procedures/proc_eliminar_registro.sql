616d712d4b0d22848d7c86aa517cf0e3dd1cc35a
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-05-20 18:17:19 -0600

    Modificaciones

--- procedures/proc_eliminar_registro.sql
+++ procedures/proc_eliminar_registro.sql
@@ -1,7 +1,6 @@
 ---Eliminacion del procedure
 DROP PROCEDURE IF EXISTS proc_eliminar_registro;
 
----Procedure para eliminar un registro de la base de datos
 DELIMITER //
 CREATE PROCEDURE proc_eliminar_registro(
    IN p_id_trabajador INT
